ea6b705fa3ce04499d849bf3ac8e810842f9a5d5
Fouad233 <fouad.badi@student.ehb.be>
2026-01-10 17:00:16 +0100

    Ticket.java verder aangemaakt met de nodige info + Ik heb ook de klass Reis al moeten maken voor de correcte verloop van het project.

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,3 +1,28 @@
 public class Ticket {
     private Passenger passenger;
+    private String klasse;
+    private Reis reis;
+
+    public Ticket(Passenger passenger, String klasse, Reis reis){
+        this.passenger = passenger;
+        this.klasse = klasse;
+        this.reis = reis;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public String getKlasse() {
+        return klasse;
+    }
+
+    public Reis getReis() {
+        return reis;
+    }
+
+    @Override
+    public String toString(){
+        return "Klasse: " + klasse;
+    }
 }
