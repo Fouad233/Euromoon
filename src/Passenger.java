0946a8288ccda84a5933f5057bdb378957548714
Fouad233 <fouad.badi@student.ehb.be>
2026-01-10 16:39:54 +0100

    Overerving gemaakt voor de passagiers: extends Person. Ik heb ook de klass Ticket al moeten maken voor de correcte verloop van het project.

---
+++ src/Passenger.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Passenger extends Person {
+    private Ticket ticket;
+
+    public Passenger(String naam, String achternaam, String rijksregisternummer, LocalDate geboortedatum) {
+        super(naam, achternaam, rijksregisternummer, geboortedatum);
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Ticket: " + (ticket != null ? ticket : "Geen");
+    }
+}
