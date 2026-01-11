9a207f5967b2ea05f4f73305bc16484f2213854b
Fouad233 <fouad.badi@student.ehb.be>
2026-01-11 23:48:04 +0100

    Hier heb ik eerst een enumeratie gemaakt voor de verschillende typen locomotief.

---
+++ src/LocomotiefType.java
@@ -0,0 +1,14 @@
+public enum LocomotiefType {
+    CLASS_373(12),
+    CLASS_374(14);
+
+    private final int maxWagons;
+
+    LocomotiefType(int maxWagons) {
+        this.maxWagons = maxWagons;
+    }
+
+    public int getMaxWagons() {
+        return maxWagons;
+    }
+}
